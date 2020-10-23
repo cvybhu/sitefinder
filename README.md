8211fa1c3d99035db87158205f5ee54552ece289
cvybhu <>
2020-10-24 00:37:41 +0200

    V1.0

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# sitefinder
-Automatically find cool available .com domains
+# Sitefinder
+Autogenerates cool .com domain names and ensures they are free with a dns lookup  
+
+# Example output:
+```
+Results:
+nodasin.com
+desedam.com
+tibibon.com
+bimebe.com
+pepisi.com
+sotolod.com
+senanot.com
+sotenop.com
+nidene.com
+dosemad.com
+banaset.com
+bedibi.com
+manetat.com
+nedesos.com
+tibelen.com
+pebidet.com
+tonidem.com
+monape.com
+nodaban.com
+lamitim.com
+bisimed.com
+pibine.com
+tabenel.com
+desoden.com
+molatim.com
+nitamed.com
+sosato.com
+tisedem.com
+```
+# Config
+You can configure the program by changing values in `src/main.rs` at lines `99` and `100`  
+`wanted_names_number`: Number of names to find  
+`concurrent_dns_lookups`: Number of site names to test at once, putting higher value here will make the program faster but your computer will be spamming the network more which might anger your DNS provider
+
+# Running
+* Install Rust
+* Download this repository
+* Run `cargo run --release` in this folder
---
+++ src/main.rs
@@ -0,0 +1,108 @@
+#![allow(clippy::needless_return)]
+use rand::{distributions::Uniform, prelude::ThreadRng, thread_rng, Rng};
+use tokio::sync::mpsc;
+
+struct Rander {
+    rng: ThreadRng,
+    vowels_dist: Uniform<usize>,
+    nonvowels_dist: Uniform<usize>,
+    name_len_dist: Uniform<usize>,
+}
+
+const VOWELS: [char; 4] = ['a', 'o', 'e', 'i'];
+const NONVOWELS: [char; 8] = ['t', 'p', 's', 'd', 'l', 'b', 'n', 'm'];
+
+impl Rander {
+    fn new() -> Self {
+        return Rander {
+            rng: thread_rng(),
+            vowels_dist: Uniform::new(0, VOWELS.len()),
+            nonvowels_dist: Uniform::new(0, NONVOWELS.len()),
+            name_len_dist: Uniform::new(6, 8),
+        };
+    }
+
+    fn get_rand_vowel(&mut self) -> char {
+        return VOWELS[self.rng.sample(self.vowels_dist)];
+    }
+
+    fn get_rand_nonvowel(&mut self) -> char {
+        return NONVOWELS[self.rng.sample(self.nonvowels_dist)];
+    }
+
+    fn get_rand_name(&mut self) -> String {
+        let name_len: usize = self.rng.sample(self.name_len_dist);
+        let mut letters: Vec<char> = Vec::with_capacity(name_len);
+
+        for i in 0..name_len {
+            if i % 2 == 0 {
+                letters.push(self.get_rand_nonvowel());
+            } else {
+                letters.push(self.get_rand_vowel());
+            }
+        }
+
+        return letters.into_iter().collect();
+    }
+}
+
+async fn is_name_taken(name: &str) -> bool {
+    let dns_lookup_future = tokio::time::timeout(
+        std::time::Duration::from_millis(500),
+        tokio::net::lookup_host(name),
+    );
+
+    match dns_lookup_future.await {
+        Ok(Ok(mut addr_iter)) => match addr_iter.next() {
+            Some(_) => return true,
+            None => return false,
+        },
+        _ => return false,
+    };
+}
+
+async fn find_name(results_sender: mpsc::Sender<String>) {
+    loop {
+        let cur_name: String = Rander::new().get_rand_name();
+
+        if !is_name_taken(&(cur_name.clone() + ".com:80")).await {
+            let _ = results_sender.send(cur_name).await.is_err();
+        }
+    }
+}
+
+async fn find_site_names(wanted_names_number: usize, concurrent_dns_lookups: usize) {
+    let (results_sender, mut results_receiver) = mpsc::channel(1);
+
+    for _ in 0..concurrent_dns_lookups {
+        let results_sender = results_sender.clone();
+        tokio::spawn(async move { find_name(results_sender).await });
+    }
+
+    let mut results: Vec<String> = Vec::new();
+    while let Some(good_name) = results_receiver.recv().await {
+        println!("Found: {}.com", good_name);
+        results.push(good_name);
+
+        if results.len() >= wanted_names_number {
+            break;
+        }
+    }
+
+    println!("\nResults:");
+    for result in results {
+        println!("{}.com", result);
+    }
+}
+
+fn main() {
+    let wanted_names_number = 64;
+    let concurrent_dns_lookups = 8;
+
+    let runtime = tokio::runtime::Runtime::new().expect("Tokio runtime creation failed!");
+    runtime.block_on(find_site_names(wanted_names_number, concurrent_dns_lookups));
+
+    std::process::exit(0);
+    // TODO - is there a better way to drop all things in runtime without waiting for them to finish?
+    // runtime.shutdown_background(); still waits for DNS lookups to finish :/
+}
